ce5c11c8d479adaa3f1c815f36cdf58e8acb3e82
Astana Plus <jamesreq456@gmail.com>
2023-05-10 12:51:51 +0600

    add 49 page

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -6,21 +6,12 @@ use Illuminate\Http\Request;
 use App\Models\Bb;
 class BbsController extends Controller
 {
-    public function index() { 
-    	$bbs = Bb::latest()->get(); 
-$s = "Объявления\r\n\r\n"; 
-foreach ($bbs as $bb) { 
-$s .= $bb->title . "\r\n"; 
-$s .= $bb->price . " руб.\r\n"; 
-$s .= "\r\n"; 
+    public function index()
+    {
+        $context = ['bbs' => Bb::latest()->get()];
+        return view('index', $context);
 }
-return response($s) 
-->header('Content-Type', 'text/plain'); 
-}
-public function detail(Bb $bb) { 
-$s = $bb->title . "\r\n\r\n"; 
-$s .= $bb->content . "\r\n"; 
-$s .= $bb->price . " руб.\r\n";
-return response($s)->header('Content-Type', 'text/plain');
+public function detail(Bb $bb) {
+        return view('detail', ['bb' => $bb]);
 }
 }
